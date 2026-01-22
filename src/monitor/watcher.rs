931a172178f7fbf23ed7464bb0ac3b4dc31094a4
Sylvain Fargier <sylvain.fargier@cern.ch>
2026-01-22 20:17:45 +0100

    :wrench: use FSEvents
     - linux support requires update

--- src/monitor/watcher.rs
+++ src/monitor/watcher.rs
@@ -20,247 +20,35 @@
 ** Author: Sylvain Fargier <fargier.sylvain@gmail.com>
 */
 
-use anyhow::{Context, Result, anyhow};
-use dashmap::DashMap;
-use libc::{POLLERR, POLLIN, nfds_t, poll, pollfd};
-use std::{
-    fmt::{self, Debug},
-    fs::read_dir,
-    io::{PipeReader, PipeWriter, Read, Write, pipe},
-    os::fd::{AsRawFd, RawFd},
-    path::Path,
-    sync::{Arc, Mutex, Weak},
-    thread::JoinHandle,
-};
+use anyhow::Result;
+use std::sync::Weak;
 
-use super::Monitor;
 use crate::{
+    monitor::Monitor,
     service::{ServiceId, Watch},
-    utils::{debug::DebugIter, libc::check},
 };
 
 #[cfg(target_os = "linux")]
-#[path = "watcher/linux.rs"]
+#[path = "watcher/inotify.rs"]
 mod private;
 
-#[cfg(any(
-    target_os = "macos",
-    target_os = "freebsd",
-    target_os = "openbsd",
-    target_os = "netbsd"
-))]
-#[path = "watcher/unix.rs"]
+#[cfg(any(target_os = "macos"))]
+#[path = "watcher/fsevents.rs"]
 mod private;
 
-use private::WatchInfo;
+pub use private::Watcher;
 
-const WAKE_WORD: u8 = b'x';
-const EXIT_WORD: u8 = b'q';
+pub trait WatcherTrait: Sized + Sync + Send {
+    fn new(monitor: Weak<Monitor>) -> Result<Self>;
 
-type WatchMap = Arc<DashMap<RawFd, Arc<WatchInfo>>>;
+    fn add(&mut self, service_id: &ServiceId, watch: &Watch) -> Result<()>;
 
-/// Directory watcher
-pub struct Watcher {
-    tx: PipeWriter,
-    join_handle: Option<JoinHandle<()>>,
-    watchs: WatchMap,
-}
-
-impl Watcher {
-    /// Create a new [Watcher] object
-    ///
-    /// This does not register services
-    pub fn new(monitor: &Arc<Monitor>) -> Result<Self> {
-        {
-            let (rx, tx) = pipe()?;
-            let mut ret = Self {
-                tx,
-                join_handle: None,
-                watchs: Arc::new(DashMap::new()),
-            };
-
-            let join_handle = {
-                let mut ctx = WatcherThreadContext {
-                    watchs: Arc::clone(&ret.watchs),
-                    rx,
-                    monitor: Arc::downgrade(monitor),
-                    buffer: vec![0; 4096],
-                };
-                std::thread::spawn(move || {
-                    ctx.run()
-                        .inspect_err(|err| tracing::error!(?err, "watcher thread error"))
-                        .unwrap_or_default()
-                })
-            };
-            ret.join_handle = Some(join_handle);
-            Ok(ret)
-        }
-    }
-
-    pub fn wake(&mut self) {
-        if let Err(err) = self.tx.write(&[WAKE_WORD]) {
-            tracing::error!(?err, "failed to send wake-word");
-        }
-    }
-
-    pub fn stop(&mut self) {
-        if let Some(join_handle) = self.join_handle.take() {
-            if let Err(err) = self.tx.write(&[EXIT_WORD]) {
-                tracing::error!(?err, "failed to send exit-word");
-            }
-            if let Err(err) = join_handle.join() {
-                tracing::error!(?err, "watcher thread join error");
-            }
-        }
-    }
-
-    pub fn add(&mut self, service_id: &ServiceId, watch: &Watch) -> Result<()> {
-        let winfo = WatchInfo::new(service_id, watch)?;
-
-        // remove duplicates
-        self.watchs
-            .retain(|_, value| &value.service_id != service_id);
-        self.watchs.insert(winfo.as_raw_fd(), Arc::new(winfo));
-        self.wake();
-
-        Ok(())
-    }
-
-    pub fn remove(&mut self, service_id: &ServiceId) {
-        self.watchs
-            .retain(|_, value| &value.service_id != service_id);
-        self.wake();
-    }
-}
-
-struct WatcherThreadContext {
-    watchs: WatchMap,
-    rx: PipeReader,
-    monitor: Weak<Monitor>,
-    buffer: Vec<u8>,
-}
-
-impl WatcherThreadContext {
-    fn make_pfds(&self) -> Vec<pollfd> {
-        let mut pfds = Vec::with_capacity(self.watchs.len() + 1);
-        for pfd in self.watchs.iter().map(|x| pollfd {
-            fd: *x.key(),
-            events: POLLIN | POLLERR,
-            revents: 0,
-        }) {
-            pfds.push(pfd);
-        }
-        pfds.push(pollfd {
-            fd: self.rx.as_raw_fd(),
-            events: POLLIN | POLLERR,
-            revents: 0,
-        });
-        pfds
-    }
-
-    fn monitor(&self) -> Result<Arc<Monitor>> {
-        self.monitor
-            .upgrade()
-            .ok_or_else(|| anyhow!("monitor has been released"))
-    }
-
-    fn get_info(&self, fd: RawFd) -> Option<Arc<WatchInfo>> {
-        self.watchs.get(&fd).map(|x| Arc::clone(&x))
-    }
-
-    pub fn run(&mut self) -> Result<()> {
-        let mut pfds = self.make_pfds();
-        let mut update_pfds = false;
-
-        loop {
-            let _span = tracing::info_span!(parent: None, "watcher").entered();
-
-            if update_pfds {
-                pfds = self.make_pfds();
-                update_pfds = false;
-            }
-
-            let mut ret = unsafe { poll(pfds.as_mut_ptr(), pfds.len() as nfds_t, -1) };
-            if ret < 0 {
-                check(ret).context("failed to poll")?;
-            }
-
-            for pfd in pfds.iter().take(pfds.len() - 1) {
-                if pfd.revents == 0 {
-                    continue;
-                }
-
-                match self.get_info(pfd.fd) {
-                    Some(info) => update_pfds = self.process(&info)?,
-                    None => update_pfds = true,
-                };
-
-                ret -= 1;
-                if ret <= 0 {
-                    break;
-                }
-            }
-
-            if pfds.last().unwrap().revents != 0 {
-                let mut wake_word = [0u8; 1];
-                if self.rx.read(&mut wake_word)? == 1 {
-                    match wake_word[0] {
-                        WAKE_WORD => {
-                            update_pfds = true;
-                            tracing::trace!("wake-word received");
-                        }
-                        EXIT_WORD => {
-                            tracing::trace!("exit requested");
-                            return Ok(());
-                        }
-                        wake_word => {
-                            tracing::error!(wake_word, "unknown wake_word received")
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    fn process(&mut self, info: &Arc<WatchInfo>) -> Result<bool> {
-        let service = match self.monitor()?.get(&info.service_id) {
-            Some(service) => service,
-            None => return Ok(false),
-        };
-
-        info.process(&service, &mut self.buffer)
-    }
-}
-
-impl Debug for Watcher {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        struct WatchWrapper<'a>(&'a WatchMap);
-
-        impl Debug for WatchWrapper<'_> {
-            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                let mut f = f.debug_list();
-                for it in self.0.iter() {
-                    f.entry(&it.service_id);
-                }
-                f.finish()
-            }
-        }
-
-        f.debug_struct("Watcher")
-            .field("services", &WatchWrapper(&self.watchs))
-            .finish()
-    }
-}
-
-impl Drop for Watcher {
-    fn drop(&mut self) {
-        self.stop();
-    }
+    fn remove(&mut self, service_id: &ServiceId);
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, time::Duration};
+    use std::{fs::File, sync::Arc, time::Duration};
 
     use crate::{
         service::{Command, Service},
@@ -297,9 +85,10 @@ mod tests {
         };
 
         std::thread::sleep(Duration::from_millis(100));
-        tracing::trace!("creating test file");
-        File::create(temp.as_ref().join("test_file"))?;
-        std::thread::sleep(Duration::from_millis(100));
+        let file = temp.as_ref().join("test_file");
+        tracing::trace!(?file, "creating test file");
+        File::create(file)?;
+        std::thread::sleep(Duration::from_millis(500));
         assert_eq!(service.info().restarts, 2);
 
         Signal::kill(getpid(), signal::SIGTERM)?;
---
+++ src/monitor/watcher/fsevents.rs
@@ -0,0 +1,256 @@
+/*
+** Copyright (C) 2025 Sylvain Fargier
+**
+** This software is provided 'as-is', without any express or implied
+** warranty.  In no event will the authors be held liable for any damages
+** arising from the use of this software.
+**
+** Permission is granted to anyone to use this software for any purpose,
+** including commercial applications, and to alter it and redistribute it
+** freely, subject to the following restrictions:
+**
+** 1. The origin of this software must not be misrepresented; you must not
+**    claim that you wrote the original software. If you use this software
+**    in a product, an acknowledgment in the product documentation would be
+**    appreciated but is not required.
+** 2. Altered source versions must be plainly marked as such, and must not be
+**    misrepresented as being the original software.
+** 3. This notice may not be removed or altered from any source distribution.
+**
+** Author: Sylvain Fargier <fargier.sylvain@gmail.com>
+*/
+
+use std::{
+    collections::HashMap,
+    ffi::{CStr, c_double, c_void},
+    fmt::Debug,
+    path::Path,
+    sync::Weak,
+};
+
+use anyhow::Result;
+
+use dispatch2::{DispatchQueue, DispatchRetained, GlobalQueueIdentifier};
+use objc2_core_foundation::{CFArray, CFString, kCFAllocatorDefault};
+use objc2_core_services::{
+    ConstFSEventStreamRef, FSEventStreamContext, FSEventStreamCreate, FSEventStreamFlushSync,
+    FSEventStreamRef, FSEventStreamRelease, FSEventStreamSetDispatchQueue, FSEventStreamStart,
+    FSEventStreamStop, kFSEventStreamCreateFlagFileEvents, kFSEventStreamEventFlagItemCreated,
+    kFSEventStreamEventFlagItemIsDir, kFSEventStreamEventFlagItemIsFile,
+    kFSEventStreamEventFlagItemModified, kFSEventStreamEventFlagItemRemoved,
+    kFSEventStreamEventFlagItemRenamed, kFSEventStreamEventIdSinceNow,
+};
+
+use crate::{
+    monitor::Monitor,
+    service::{ServiceId, Watch},
+    utils::debug::DebugIter,
+};
+
+use super::WatcherTrait;
+
+pub type Watcher = FSEventWatcher;
+
+/// A [WatcherTrait] object that relies on MacOS FSEvents
+pub struct FSEventWatcher {
+    monitor: Weak<Monitor>,
+    queue: DispatchRetained<DispatchQueue>,
+    watchs: HashMap<ServiceId, WatchInfo>,
+}
+
+impl Debug for FSEventWatcher {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Watcher")
+            .field("services", &DebugIter::new(self.watchs.keys()))
+            .finish()
+    }
+}
+
+impl WatcherTrait for FSEventWatcher {
+    fn new(monitor: Weak<Monitor>) -> Result<Self> {
+        // using a dedicated queue will spawn a thread
+        // using [DispatchQueue::main] will wait until someone calls [dispatch2::dispatch_main]
+        // [FSEventStreamUnscheduleFromRunLoop] is deprecated and would work if we call [CRunLoop::run]
+        Ok(Self {
+            monitor,
+            queue: DispatchQueue::global_queue(GlobalQueueIdentifier::QualityOfService(
+                dispatch2::DispatchQoS::Default,
+            )),
+            watchs: Default::default(),
+        })
+    }
+
+    fn add(&mut self, service_id: &ServiceId, watch: &Watch) -> Result<()> {
+        self.watchs.insert(
+            *service_id,
+            WatchInfo::new(service_id, watch, self.queue.as_ref(), self.monitor.clone()),
+        );
+        Ok(())
+    }
+
+    fn remove(&mut self, service_id: &ServiceId) {
+        self.watchs.remove(service_id);
+    }
+}
+
+bitflags::bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct FSEventFlags: u32 {
+        const CREATED  = kFSEventStreamEventFlagItemCreated;
+        const REMOVED  = kFSEventStreamEventFlagItemRemoved;
+        const MODIFIED = kFSEventStreamEventFlagItemModified;
+        const RENAMED  = kFSEventStreamEventFlagItemRenamed;
+        const IS_FILE  = kFSEventStreamEventFlagItemIsFile;
+        const IS_DIR   = kFSEventStreamEventFlagItemIsDir;
+    }
+}
+
+struct WatchInfoData {
+    service_id: ServiceId,
+    monitor: Weak<Monitor>,
+}
+
+impl WatchInfoData {
+    pub fn new(service_id: ServiceId, monitor: Weak<Monitor>) -> Box<Self> {
+        Box::new(Self {
+            service_id,
+            monitor,
+        })
+    }
+
+    pub fn process(&self, path: &str, flags: FSEventFlags) {
+        let monitor = match self.monitor.upgrade() {
+            Some(m) => m,
+            None => {
+                tracing::error!("monitor has been released");
+                return;
+            }
+        };
+        let service = match monitor.get(&self.service_id) {
+            Some(service) => service,
+            None => {
+                tracing::error!(id = self.service_id, "unknown service");
+                return;
+            }
+        };
+        if service
+            .watch
+            .as_ref()
+            .is_some_and(|w| !w.is_excluded(Path::new(path)))
+        {
+            tracing::info!(id=service.id, name=service.name, file=?path,
+                event=?flags,
+                "file event detected");
+            service.restart();
+        } else {
+            tracing::trace!(id=service.id, name=service.name, file=?path, event=?flags,
+                "file event rejected")
+        }
+    }
+
+    pub fn as_void(&self) -> *mut c_void {
+        self as *const _ as *mut c_void
+    }
+}
+
+struct WatchInfo {
+    stream: FSEventStreamRef,
+    // code is not dead, it's sent using raw-pointer to the callback
+    #[allow(dead_code)]
+    data: Box<WatchInfoData>,
+}
+
+impl WatchInfo {
+    pub fn new(
+        service_id: &ServiceId,
+        watch: &Watch,
+        queue: &DispatchQueue,
+        monitor: Weak<Monitor>,
+    ) -> Self {
+        let paths = CFArray::from_retained_objects(
+            &watch
+                .paths
+                .iter()
+                .filter_map(|path| {
+                    if watch.is_excluded(path) {
+                        tracing::warn!(
+                            ?path,
+                            "configured path is excluded, add it to the `include` list"
+                        );
+                        None
+                    } else {
+                        tracing::trace!(?path, "adding watch");
+                        Some(CFString::from_str(
+                            path.as_os_str().to_str().expect("invalid path"),
+                        ))
+                    }
+                })
+                .collect::<Vec<_>>(),
+        );
+        let data = WatchInfoData::new(*service_id, monitor);
+        let mut context = FSEventStreamContext {
+            version: 0,
+            info: data.as_void(),
+            retain: None,
+            release: None,
+            copyDescription: None,
+        };
+        unsafe {
+            let stream = FSEventStreamCreate(
+                kCFAllocatorDefault,
+                Some(Self::on_events),
+                &mut context,
+                paths.as_ref(),
+                kFSEventStreamEventIdSinceNow,
+                0.2 as c_double,
+                kFSEventStreamCreateFlagFileEvents,
+            );
+            FSEventStreamSetDispatchQueue(stream, Some(queue));
+            FSEventStreamStart(stream);
+            Self { stream, data }
+        }
+    }
+
+    extern "C-unwind" fn on_events(
+        _stream_ref: ConstFSEventStreamRef,
+        info: *mut c_void,
+        num_events: usize,
+        event_paths: std::ptr::NonNull<c_void>,
+        event_flags: std::ptr::NonNull<u32>,
+        _event_ids: std::ptr::NonNull<u64>,
+    ) {
+        let data = unsafe { &*(info as *const WatchInfoData) };
+        let event_paths = unsafe {
+            core::slice::from_raw_parts(event_paths.as_ptr() as *const *const i8, num_events)
+        };
+        let event_flags = unsafe { core::slice::from_raw_parts(event_flags.as_ptr(), num_events) };
+
+        for (&path, &flags) in event_paths.iter().zip(event_flags) {
+            match unsafe { CStr::from_ptr(path) }.to_str() {
+                Ok(path) => data.process(path, FSEventFlags::from_bits_truncate(flags)),
+                Err(err) => tracing::error!(?err, "invalid utf8 string for path"),
+            }
+        }
+    }
+
+    pub fn stop(&mut self) {
+        if !self.stream.is_null() {
+            unsafe {
+                FSEventStreamFlushSync(self.stream);
+                FSEventStreamStop(self.stream);
+                FSEventStreamSetDispatchQueue(self.stream, None);
+                FSEventStreamRelease(self.stream);
+                self.stream = std::ptr::null_mut();
+            }
+        }
+    }
+}
+
+impl Drop for WatchInfo {
+    fn drop(&mut self) {
+        self.stop();
+    }
+}
+
+unsafe impl Send for WatchInfo {}
+unsafe impl Sync for WatchInfo {}
